11dd7977d7e0ef4ca6c24bcde0390ee5f1f2fa2f
culjakluk <llukaculjak@gmail.com>
2020-12-14 19:58:24 +0100

    added monster generator

--- OOP_and_architecture/OOP_and_architecture.Presentation/Program.cs
+++ OOP_and_architecture/OOP_and_architecture.Presentation/Program.cs
@@ -1,5 +1,7 @@
 ﻿using OOP_and_architecture.Data;
+using OOP_and_architecture.Data.Monsters;
 using System;
+using System.Collections.Generic;
 
 namespace OOP_and_architecture.Presentation
 {
@@ -9,20 +11,41 @@ namespace OOP_and_architecture.Presentation
         {
 
             int action;
+            string heroName;
+            var monsterCollection = new Dictionary<int, Monster>();
+            MonsterGenerator(monsterCollection);
             do
             {
                 Console.WriteLine("\tGlavni izbornik:\n" +
-                    "1. Odabir Heroja: "); ;
+                    "1. Odabir Heroja: \n" +
+                    "0. Izlazak iz igre"); ;
                 action = int.Parse(Console.ReadLine());
                 switch (action)
-                { 
+                {
                     case 1:
-                        SelectHeroType();
+                        var heroSelected = SelectHeroType();
+                        switch (heroSelected)
+                        {
+                            case 1:
+                                heroName = Input("Unesite ime vaseg warriora: ");
+                                var heroWarrior = new Warrior(heroName);
+                                break;
+                            case 2:
+                                heroName = Input("Unesite ime vaseg magea: ");
+                                var heroMage = new Mage(heroName);
+                                break;
+                            case 3:
+                                heroName = Input("Unesite ime vaseg rangera: ");
+                                var hero = new Ranger(heroName);
+                                break;
+                            default:
+                                break;
+                        }
                         break;
                     default:
                         break;
                 }
-            } while (action!=0);            
+            } while (action != 0);
         }
         static string Input(string message)
         {
@@ -34,7 +57,7 @@ namespace OOP_and_architecture.Presentation
             } while (!string.IsNullOrWhiteSpace(userInput));
             return userInput;
         }
-        static int SelectHeroType() 
+        static int SelectHeroType()
         {
             int heroType;
             var warrior = new Warrior("placeholder");
@@ -47,34 +70,69 @@ namespace OOP_and_architecture.Presentation
                    $"0. povratak u glavni izbornik");
             int action;
             var passedOnce = false;
+
             do
             {
                 do
                 {
-                if(passedOnce)
+                    if (passedOnce)
                     {
                         Console.WriteLine("Unesite broj ispred tipa heroja(0 za povratak): ");
                     }
                     action = int.Parse(Console.ReadLine());
                     passedOnce = true;
-                } while ( action < -1 || 3 < action );
+                } while (action < -1 || 3 < action);
+
                 switch (action)
                 {
                     case 1:
                         heroType = 1;
-                        break;
+                        return heroType;
                     case 2:
                         heroType = 2;
-                        break;
+                        return heroType;
                     case 3:
                         heroType = 3;
-                        break;
+                        return heroType;
                     default:
                         heroType = 0;
                         break;
                 }
-            } while (action!=0);
+            } while (action != 0);
             return heroType;
+        }
+        static void MonsterGenerator(Dictionary<int, Monster> monsterCollection)
+        {
+            var index = 0;
+            var randomNumber = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                GenerateMonster(monsterCollection, randomNumber.Next(), ref index);
+            }
+        }
+        static void GenerateMonster(Dictionary<int, Monster> monsterCollection, int whichMonster, ref int index)
+        {  
+            var monster = new Monster();
+            switch (whichMonster)
+            {
+                case 1:
+                    monster = new Goblin();
+                    monsterCollection.Add(index, monster);
+                    index++;
+                    break;
+                case 2:
+                    monster = new Witch();
+                    monsterCollection.Add(index, monster);
+                    index++;
+                    break;
+                case 3:
+                    monster = new Brute();
+                    monsterCollection.Add(index, monster);
+                    index++;
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
